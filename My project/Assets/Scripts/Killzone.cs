b50546f6ca7bfde9bc5959b248e25bb81190fdba
alexkeener318 <alexkeener318@gmail.com>
2026-02-22 00:02:25 -0600

    Fixes dying

--- My project/Assets/Scripts/Killzone.cs
+++ My project/Assets/Scripts/Killzone.cs
@@ -2,11 +2,25 @@ using UnityEngine;
 
 public class Killzone : MonoBehaviour
 {
+    private DeathMenu deathMenu;
+
+    void Start()
+    {
+        deathMenu = FindFirstObjectByType<DeathMenu>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") || other.name == "Ghost")
+        if (other.CompareTag("Player"))
         {
-            // respawn or destroy
+            if (deathMenu != null)
+            {
+                Destroy(other.gameObject);
+                deathMenu.ShowDeathMenu();
+            }
+        }
+        else if (other.name == "Ghost")
+        {
             Destroy(other.gameObject);
         }
     }
